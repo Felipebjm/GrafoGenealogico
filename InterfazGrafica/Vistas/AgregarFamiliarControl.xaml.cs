fde2d5a78ac57b0c9d0d1db635c68e06bebca0ab
Felipebjm <febejaranom@gmail.com>
2025-11-06 19:20:10 -0600

    UserControl: AgregarFamiliar

--- InterfazGrafica/Vistas/AgregarFamiliarControl.xaml.cs
+++ InterfazGrafica/Vistas/AgregarFamiliarControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32; //Necesario para el OpenFileDialog
 
 namespace InterfazGrafica.Vistas
 {
@@ -20,9 +21,31 @@ namespace InterfazGrafica.Vistas
     /// </summary>
     public partial class AgregarFamiliarControl : UserControl
     {
+        // Más adelante podés recibir el grafo por constructor
+        // private GrafoFamilia _grafo;
+
         public AgregarFamiliarControl()
         {
             InitializeComponent();
+        }
+
+        private void BtnElegirPosicion_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí abrirías la ventana "SeleccionarPosicionWindow"
+            // y cuando el usuario haga clic, te devuelve X/Y
+            // y vos las ponés en TxtX.Text y TxtY.Text
+        }
+
+        private void BtnSeleccionarFoto_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí abrís un OpenFileDialog y cargas la imagen en ImgFoto.Source
+        }
+
+        private void BtnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            // Aquí leés todos los campos,
+            // creás un objeto Familiar y se lo pasás al grafo
+            // (esto lo haremos cuando tengamos el proyecto Core referenciado)
         }
     }
 }
