39d5c628add7de43ac379fb436b708153abd01b9
Felipebjm <febejaranom@gmail.com>
2025-11-20 20:19:50 -0600

    Atributo:AnioFallecimiento

--- Clases/Persona.cs
+++ Clases/Persona.cs
@@ -16,6 +16,7 @@ namespace Clases
         public string? RutaFoto { get; set; }         // Ruta a la imagen de la persona
         public double PosX { get; set; }              // Coordenada X para visualización
         public double PosY { get; set; }              // Coordenada Y para visualización
+        public int? AnioFallecimiento { get; set; }   // Año de fallecimiento (null si esta vivo)
 
         // Propiedad calculada: edad en años
         public int Edad => (int)((DateTime.Now - FechaNacimiento).TotalDays / 365.25);
@@ -24,16 +25,29 @@ namespace Clases
 
 
         public Persona(string nombre, int cedula, DateTime fechaNacimiento, bool estaVivo,
-                       string? rutaFoto = null, double posX = 0, double posY = 0)
+                       string? rutaFoto = null, double posX = 0, double posY = 0, int? anioFallecimiento = null)
         {
             //Validaciones
-            if(string.IsNullOrWhiteSpace(nombre))
-                throw new ArgumentException("El nombre no puede estar vacío.", nameof(nombre));
+            if (string.IsNullOrWhiteSpace(nombre))
+                throw new ArgumentException();
             if (cedula <= 0)
-                throw new ArgumentException("La cédula debe ser un número positivo.", nameof(cedula));
+                throw new ArgumentException();
             if (fechaNacimiento > DateTime.Now)
-                throw new ArgumentException("La fecha de nacimiento no puede ser en el futuro.", nameof(fechaNacimiento));
-
+                throw new ArgumentException();
+            // Validaciones relacionada con el fallecimientos
+            if (!estaVivo)
+            {
+                if (!anioFallecimiento.HasValue)
+                    throw new ArgumentException();
+                if (anioFallecimiento.Value < fechaNacimiento.Year)
+                    throw new ArgumentException();
+                if (anioFallecimiento.Value > DateTime.Now.Year)
+                    throw new ArgumentException();
+            }
+            else
+            {
+                anioFallecimiento = null;
+            }
 
             Id = Guid.NewGuid();
             Nombre = nombre;
@@ -44,6 +58,7 @@ namespace Clases
             RutaFoto = rutaFoto;
             PosX = posX;
             PosY = posY;
+            AnioFallecimiento = anioFallecimiento;
         }
 
         public override string ToString()
